8951d65c7054aaaec20d9ae3e208c2ee60e73565
Ertuğrul AKKAYA <ertuakkaya@gmail.com>
2026-03-12 10:04:36 +0300

    remove platform and greeting

--- composeApp/src/androidMain/kotlin/com/ertugrulakkaya/spaceexplorer/Platform.android.kt
+++
@@ -1,9 +0,0 @@
-package com.ertugrulakkaya.spaceexplorer
-
-import android.os.Build
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- composeApp/src/commonMain/kotlin/com/ertugrulakkaya/spaceexplorer/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.ertugrulakkaya.spaceexplorer
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
